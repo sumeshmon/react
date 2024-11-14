e73db433953f1b6ad280597282cf80e9bba0bef2
ssundaran <sumesh.sundaran@eqs.com>
2024-11-14 09:09:47 +0530

    custom header

---
+++ 01basicreact/src/Header.js
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <>
+            <header>
+                <h1>My first header using react</h1>
+            </header>
+        </>
+    )
+}
+export default Header 
