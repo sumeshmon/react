1ef22f9a222f8c5b7bf4e8f5487b79f644c6d1cc
ssundaran <sumesh.sundaran@eqs.com>
2025-03-27 07:58:29 +0530

    vite dom

--- play-ground/src/index.jsx
+++ play-ground/src/index.jsx
@@ -4,8 +4,20 @@ const root = createRoot(document.getElementById("root"))
 function MainContent() {
     return <h1>React is great!</h1>
 }
+function OrderTest() {
+    return <h2>Order test</h2>
+}
 root.render(
     <div>
+        <OrderTest/>
         <MainContent />
     </div>
 )
+
+// with vanila
+const element = document.createElement('h2')
+element.className = 'header'
+element.innerText = 'i am from vanila'
+document.body.appendChild(element)
+
+
