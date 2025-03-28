79cd90da820ffefbc6033430af9b3347d304d811
ssundaran <sumesh.sundaran@eqs.com>
2025-03-28 07:15:42 +0530

    try to import images

--- play-ground/src/index.jsx
+++ play-ground/src/index.jsx
@@ -2,15 +2,20 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
 function MainContent() {
-    return <h1>React is great!</h1>
+    return (
+        <>
+            <h1>React is great!</h1>
+            <p>This is para</p>
+        </>
+    
+    )
+    
 }
-function OrderTest() {
-    return <h2>Order test</h2>
-}
+
 root.render(
     <div>
-        <OrderTest/>
-        <MainContent />
+        <MainContent/>
+        <img src='/logo.png' alt="My React " className='logo'/>   
     </div>
 )
 
@@ -19,5 +24,3 @@ const element = document.createElement('h2')
 element.className = 'header'
 element.innerText = 'i am from vanila'
 document.body.appendChild(element)
-
-
